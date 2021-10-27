cac036f09364122dd5aaec434ad71e25ba75c5b6
Gabrieljardim12 <91630991+Gabrieljardim12@users.noreply.github.com>
2021-10-27 15:13:48 -0300

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,8 +1,7 @@
-var starImg,bgImg, fada, fairyImage;
-var star, starBody, vozFada;
+var starImg,bgImg, imgFada;
+var star, starBody;
+var vozFada, fada;
 //criar variável para sprite de fada e imgFada
-
-
 
 const Engine = Matter.Engine;
 const World = Matter.World;
@@ -13,9 +12,10 @@ function preload()
 {
     starImg = loadImage("images/star.png");
 	bgImg = loadImage("images/starNight.png");
-    fairyImage = loadAnimation("images/fairyImage1.png", "images/fairyImage2.png" );
-    vozFada = loadSound ("sound/JoyMusic.mp3")
-    
+    //para carregar imagens e sons
+    imgFada = loadAnimation("images/fairyImage1.png","images/fairyImage2.png");
+    vozFada = loadSound("sound/JoyMusic.mp3");
+
     //carregar animação de fada 
 }
 
@@ -23,11 +23,11 @@ function setup() {
     createCanvas(800, 750);
 
     //escrever código para tocar o som vozFada
+    //vozFada.play();
 
     //criar sprite de fada e adicionar animação para fada
-
-    fada = createSprite(400, 400);
-    fada.addAnimation("AnimacaoDaFada ", fairyImage);
+    fada = createSprite(600,500);
+    fada.addAnimation("fada",imgFada);
 	fada.scale = 0.2;
 
     star = createSprite(650,30);
@@ -37,30 +37,34 @@ function setup() {
     engine = Engine.create();
 	world = engine.world;
 
-	starBody = Bodies.circle(650 , 30 , 5 , {restitution:0.5, isStatic:true});
+	starBody = Bodies.circle(650 ,30 , 5 , {restitution:0.5, isStatic:true});
 	World.add(world, starBody);
 	
 	Engine.run(engine);
 
 }
+
 function draw(){
-
-    background  (0);
-
+    background(bgImg);
+   
     vozFada.play();
 
-
-if(keyDown("left")){
-    fada.x = fada.x -2 ;
-}
+    if(keyDown("left")){
+        fada.x = fada.x -3;
+    }
     if(keyDown("right")){
-        fada.x = fada.x +2 ;
+        fada.x = fada.x +3;
     }
 
-    if(star.y > 470 && starBody .position.y > 470){
-        Matter.Body.setStatic(starBody, true);
+    if(keyDown("DOWN_ARROW")) {
+        star.x = starBody.position.x;
+        star.y = starBody.position.y;
+        Matter.Body.setStatic(starBody,false);
     }
+    if(star.y > 470 && starBody.position.y > 470 ){
+        Matter.Body.setStatic(starBody,true);
+    }
+        
 
-
-drawSprites();
+    drawSprites();
 }
